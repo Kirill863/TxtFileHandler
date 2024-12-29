437104885f8f202be014b0b6d4e8e6be68dd9c64
Kirill863 <Byblik4310@gmail.com>
2024-12-29 19:29:52 +0300

    добаыил метод дл записи данных

--- TxtFileHandler.py
+++ TxtFileHandler.py
@@ -11,3 +11,12 @@ class TxtFileHandler
         except Exception as e:
             print(f"Ошибка при чтении файла: {e}")
             return ""
+    @staticmethod
+    def write_file(filepath: str, *data: str) -> None:
+        """Метод для записи данных в TXT файл."""
+        try:
+            with open(filepath, 'w', encoding='utf-8') as file:
+                for line in data:
+                    file.write(line)
+        except Exception as e:
+            print(f"Ошибка при записи в файл: {e}")
